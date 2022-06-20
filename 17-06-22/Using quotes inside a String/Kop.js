ca9d9e7accceb01a9a280db77937baa931585625
Srijan Baniyal <107460791+Rambo9901@users.noreply.github.com>
2022-06-20 17:42:09 +0530

    Create Kop.js

---
+++ 17-06-22/Using quotes inside a String/Kop.js
@@ -0,0 +1,9 @@
+var titles;
+var messages;
+title = "Molly's Special Offers";
+message = '<a href="sale.html\">25% off !!</a>';
+
+var elTitle = document.getElementById('title');
+elTitle.innterHTML = title;
+var elNote = document.getElementById('note');
+elNOte.innerHTML = message;
