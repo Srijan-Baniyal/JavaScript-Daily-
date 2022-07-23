5d2df78f483e7b0b3f4ddcf0c7948c2d3da2da54
Srijan Baniyal <107460791+Rambo9901@users.noreply.github.com>
2022-07-23 17:40:03 +0530

    Delete 23-07-22/Previous and Next Sibling directory

--- 23-07-22/Previous and Next Sibling/j.js
+++
@@ -1,6 +0,0 @@
-let startItem = document.getElementById('two');
-let prevItem = startItem.previousSibling;
-let nextItem = startItem.nextSibling;
-
-prevItem.className = 'complete';
-nextItem.className = 'cool';
