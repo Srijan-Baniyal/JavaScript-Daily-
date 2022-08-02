b9ea828bed480a23c1c2d2c930fb6b4e5c35baf9
Srijan Baniyal <107460791+Rambo9901@users.noreply.github.com>
2022-08-02 19:31:05 +0530

    Add files via upload

---
+++ 02-08-22/Using Parameters With Event Listners/eminen.js
@@ -0,0 +1,14 @@
+let elUsername = document.getElementById('username');   
+let elMsg      = document.getElementById('feedback');   
+
+function checkUsername(minLength) {                     
+  if (elUsername.value.length < minLength) {            
+    elMsg.innerHTML = 'Username must be ' + minLength + ' characters or more';
+  } else {                                             
+    elMsg.innerHTML = '';                              
+  }
+}
+
+elUsername.addEventListener('blur', function() {        
+  checkUsername(5);                                     
+}, false);
